277b27244ab54ac100eda53cd36ec6c3b67fdd3c
mbp13 <shaxobiddina20@gmail.com>
2023-03-08 04:21:26 +0500

    gg

--- data/config.py
+++ data/config.py
@@ -4,6 +4,6 @@ from environs import Env
 env = Env()
 env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+BOT_TOKEN = env.str("5970958544:AAFhTMWiNUgjg9U6uNqqLmmw4BS9jt8te0E")  # Bot toekn
+ADMINS = env.list("1218525470")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
